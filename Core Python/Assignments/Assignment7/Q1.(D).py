c8d3030a7d0f12b82c807856f33946c6c9d813e5
Ankita Shinde <ankitashinde28523@gmail.com>
2026-07-26 19:40:25 +0530

    Add files via upload
    This repository showcases my solution for Python Assignment - 7 (Patterns) from FirstBitSolutions. The programs include a variety of pattern printing exercises that improve programming fundamentals , loop concepts, and logical reasoning in python.

---
+++ Core Python/Assignments/Assignment7/Q1.(D).py
@@ -0,0 +1,23 @@
+# WAP to print pattern:
+# D)
+
+for i in range(1,6):
+    for j in range(1,6-i):
+        print(' ',end= ' ')
+ 
+    num = i 
+    for j in range(1,i+1):
+        print(num,end=' ')
+        num= num+1
+
+    num= num-2
+    for j in range(1,i):
+        print(num,end=' ')
+        num=num-1
+    print()
+
+     
+
+
+       
+ 
