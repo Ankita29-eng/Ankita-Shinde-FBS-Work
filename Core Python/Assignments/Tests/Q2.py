46ae0930815b425430304e0ecf0430bc51dbc590
Ankita Shinde <ankitashinde28523@gmail.com>
2026-08-10 16:58:29 +0530

    Add files via upload
    Test1 on core python

---
+++ Core Python/Assignments/Tests/Q2.py
@@ -0,0 +1,7 @@
+#2. Write a program to calculate simple interest based on Principal, Rate and Time
+#(SI = P*R*T/100)
+p =float(input('Enter Principal:'))
+R = float(input('Enter Rate:'))
+T = float(input('Enter Time:'))
+SI= (p*R*T)/100
+print("Simple Interest=",SI)
