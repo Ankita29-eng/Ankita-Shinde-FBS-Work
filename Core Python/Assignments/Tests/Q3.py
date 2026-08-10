44128f99da3b2102dfd0c7a3bbdfe22c3e3f53c6
Ankita Shinde <ankitashinde28523@gmail.com>
2026-08-10 17:21:18 +0530

    Delete Core Python/Assignments/Tests directory

--- Core Python/Assignments/Tests/Q3.py
+++
@@ -1,7 +0,0 @@
-#Write a program to accept distance in km and convert it into meters and
-#centimeters both.
-km = float(input ('Enter distance in km:'))
-meters = km *1000
-centimeters = km *100000
-print("Distance in meter: ",meters)
-print("Distance in centimeters :",centimeters)
